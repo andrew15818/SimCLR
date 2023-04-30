876bcc64cd0bdf7f937c5aa2b8249f5bb0791671
Andres Ponce <andreseeponce@gmail.com>
2023-04-30 13:26:13 +0800

    simplified cifar10 dataloader

--- data.py
+++ data.py
@@ -57,6 +57,7 @@ class ImbalanceCIFAR10(torchvision.datasets.CIFAR10):
         self.shuffleClasses = shuffleClasses
         self.gen_imbalanced_data(img_num_list)
         self.img_num_list = img_num_list
+        self.cls_num = 10 
 
     def get_img_num_per_cls(self, cls_num, imb_type, imb_factor):
         img_max = len(self.data) / cls_num
@@ -95,7 +96,7 @@ class ImbalanceCIFAR10(torchvision.datasets.CIFAR10):
         self.targets = new_targets
          
         # Save current dataset so we know Many, Medium, Few classes
-        np.save(f'splits/cifar{cls_num}_imb.npy', np.array(self.targets))
+        np.save(f'splits/cifar{self.cls_num}_imb.npy', np.array(self.targets))
 
     def get_cls_num_dict(self):
         #cls_num_list = []
@@ -114,69 +115,69 @@ class ImbalanceCIFAR10(torchvision.datasets.CIFAR10):
         x2 = self.transform(img)
         return [x1, x2], target
 
-class ImbalanceCIFAR100(torchvision.datasets.CIFAR100):
+class ImbalanceCIFAR100(ImbalanceCIFAR10):
     cls_num = 100
-    def __init__(self, root, imb_type='exp', imb_factor=0.01, train=True,
-                 transform=None, target_transform=None, download=False):
-        super(ImbalanceCIFAR100, self).__init__(root, train, transform, target_transform, download)
-        img_num_list = self.get_img_num_per_cls(self.cls_num, imb_type, imb_factor)
-        self.gen_imbalanced_data(img_num_list)
-        self.img_num_list = img_num_list
+    #def __init__(self, root, imb_type='exp', imb_factor=0.01, train=True,
+    #             transform=None, target_transform=None, download=False):
+    #    super(ImbalanceCIFAR100, self).__init__(root, train, transform, target_transform, download)
+    #    img_num_list = self.get_img_num_per_cls(self.cls_num, imb_type, imb_factor)
+    #    self.gen_imbalanced_data(img_num_list)
+    #    self.img_num_list = img_num_list
 
-    def get_img_num_per_cls(self, cls_num, imb_type, imb_factor):
-        img_max = len(self.data) / cls_num
-        img_num_per_cls = []
-        if imb_type == 'exp':
-            for cls_idx in range(cls_num):
-                num = img_max * (imb_factor**(cls_idx / (cls_num - 1.0)))
-                img_num_per_cls.append(int(num))
-        elif imb_type == 'step':
-            for cls_idx in range(cls_num // 2):
-                img_num_per_cls.append(int(img_max))
-            for cls_idx in range(cls_num // 2):
-                img_num_per_cls.append(int(img_max * imb_factor))
-        else:
-            img_num_per_cls.extend([int(img_max)] * cls_num)
-        return img_num_per_cls
+    #def get_img_num_per_cls(self, cls_num, imb_type, imb_factor):
+    #    img_max = len(self.data) / cls_num
+    #    img_num_per_cls = []
+    #    if imb_type == 'exp':
+    #        for cls_idx in range(cls_num):
+    #            num = img_max * (imb_factor**(cls_idx / (cls_num - 1.0)))
+    #            img_num_per_cls.append(int(num))
+    #    elif imb_type == 'step':
+    #        for cls_idx in range(cls_num // 2):
+    #            img_num_per_cls.append(int(img_max))
+    #        for cls_idx in range(cls_num // 2):
+    #            img_num_per_cls.append(int(img_max * imb_factor))
+    #    else:
+    #        img_num_per_cls.extend([int(img_max)] * cls_num)
+    #    return img_num_per_cls
 
-    def gen_imbalanced_data(self, img_num_per_cls):
-        new_data = []
-        new_targets = []
-        targets_np = np.array(self.targets, dtype=np.int64)
-        classes = np.unique(targets_np)
-        # np.random.shuffle(classes)
-        self.num_per_cls_dict = dict()
-        for the_class, the_img_num in zip(classes, img_num_per_cls):
-            self.num_per_cls_dict[the_class] = the_img_num
-            idx = np.where(targets_np == the_class)[0]
-            np.random.shuffle(idx)
-            selec_idx = idx[:the_img_num]
-            new_data.append(self.data[selec_idx, ...])
-            new_targets.extend([the_class, ] * the_img_num)
-        new_data = np.vstack(new_data)
-        self.data = new_data
-        self.targets = new_targets
-        print(np.array(self.targets).shape)
-    def get_cls_num_dict(self):
-        cls_num_dict = {}
-        for i in range(self.cls_num):
-            cls_num_list[i] = self.num_per_cls_dict[i].append(self.num_per_cls_dict[i])
-        return cls_num_list
+    #def gen_imbalanced_data(self, img_num_per_cls):
+    #    new_data = []
+    #    new_targets = []
+    #    targets_np = np.array(self.targets, dtype=np.int64)
+    #    classes = np.unique(targets_np)
+    #    # np.random.shuffle(classes)
+    #    self.num_per_cls_dict = dict()
+    #    for the_class, the_img_num in zip(classes, img_num_per_cls):
+    #        self.num_per_cls_dict[the_class] = the_img_num
+    #        idx = np.where(targets_np == the_class)[0]
+    #        np.random.shuffle(idx)
+    #        selec_idx = idx[:the_img_num]
+    #        new_data.append(self.data[selec_idx, ...])
+    #        new_targets.extend([the_class, ] * the_img_num)
+    #    new_data = np.vstack(new_data)
+    #    self.data = new_data
+    #    self.targets = new_targets
+    #    print(np.array(self.targets).shape)
+    #def get_cls_num_dict(self):
+    #    cls_num_dict = {}
+    #    for i in range(self.cls_num):
+    #        cls_num_list[i] = self.num_per_cls_dict[i].append(self.num_per_cls_dict[i])
+    #    return cls_num_list
 
-    def __getitem__(self, idx):
-        img = self.data[idx]
-        target = self.targets[idx]
+    #def __getitem__(self, idx):
+    #    img = self.data[idx]
+    #    target = self.targets[idx]
 
-        img = Image.fromarray(img)
+    #    img = Image.fromarray(img)
 
-        x1 = self.transform(img)
-        x2 = self.transform(img)
-        return [x1, x2], target
+    #    x1 = self.transform(img)
+    #    x2 = self.transform(img)
+    #    return [x1, x2], target
 
 # Debug
 if __name__=='__main__':
     trainaugs, testaugs = get_transforms()
-    dataset = ImbalanceCIFAR10(root='../datasets', 
+    dataset = ImbalanceCIFAR100(root='../datasets', 
                             train=True, 
                             transform=trainaugs)
     print(dataset.img_num_list)
