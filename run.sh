4a8ee7f135deeba1a4c903ba1696eec91795bb37
Andres Ponce <andreseeponce@gmail.com>
2023-05-09 12:31:40 +0800

    cifar10, resuming from 1200 checkpoint

--- run.sh
+++ run.sh
@@ -3,11 +3,12 @@ python train.py \
 	--data-dir ../datasets \
 	--arch resnet18 \
 	--batch-size 512 \
-	--dataset cifar10 \
-	--num-classes 10 \
+	--dataset cifar100 \
+	--num-classes 100 \
 	--optimizer adam \
 	--lr .0003 \
 	--encoder_dim 512 \
 	--temperature 0.2 \
 	--proj_hid_dim 128 \
-	--epochs 1200 \
+	--resume runs/cifar100_1200_normreweight.pth.tar \
+	--epochs 1500 \
